6e92867f45941212670c5867bd1d0791892deac6
Kyle Flynn <kyle.flynn5798@yahoo.com>
2017-10-09 16:51:53 -0400

    Added firebase functionality and started user auth.

--- app/src/main/java/edu/gvsu/cis/activityapp/LoginActivity.java
+++ app/src/main/java/edu/gvsu/cis/activityapp/LoginActivity.java
@@ -1,13 +1,52 @@
 package edu.gvsu.cis.activityapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            // Transition to the main UI.
+        } else {
+            // Continue with this activity.
+        }
+    }
+
+    private boolean validCredentials(String email, String password) {
+        // TODO - Test email regex and make sure password is valid.
+        return email != null && password != null;
+    }
+
+    private void createUser(String email, String password) {
+        if (validCredentials(email, password)) {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener((@NonNull Task<AuthResult> task) -> {
+                if (task.isSuccessful()) {
+                    FirebaseUser currentUser = mAuth.getCurrentUser();
+                    // Transition to the main UI.
+                } else {
+                    Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT);
+                }
+            });
+        }
+    }
+
 }
