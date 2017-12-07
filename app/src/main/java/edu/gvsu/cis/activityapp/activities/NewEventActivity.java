7f2bea77bf4a234eaa9addc09355569ffceee184
Kyle Flynn <kyle.flynn5798@yahoo.com>
2017-12-07 12:24:57 -0500

    Added place picking functionality.

--- app/src/main/java/edu/gvsu/cis/activityapp/activities/NewEventActivity.java
+++ app/src/main/java/edu/gvsu/cis/activityapp/activities/NewEventActivity.java
@@ -1,17 +1,28 @@
 package edu.gvsu.cis.activityapp.activities;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceLikelihoodBufferResponse;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
@@ -27,7 +38,11 @@ import java.util.Calendar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import edu.gvsu.cis.activityapp.R;
+import edu.gvsu.cis.activityapp.services.GooglePlacesProvider;
+import edu.gvsu.cis.activityapp.util.HttpRequest;
+import edu.gvsu.cis.activityapp.util.MapManager;
 import edu.gvsu.cis.activityapp.util.PlaceEvent;
+import edu.gvsu.cis.activityapp.util.RequestBuilder;
 
 public class NewEventActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, DatePickerDialog.OnDateSetListener{
 
@@ -41,10 +56,15 @@ public class NewEventActivity extends AppCompatActivity implements TimePickerDia
     CheckBox privateEvent;
 
     DateTime date;
-
     DateTime time;
+    Place currentPlace;
 
-
+    @BindView(R.id.btnUseMyLoc)
+    Button useMyLoc;
+    @BindView(R.id.btnSelectLoc)
+    Button selectLoc;
+    @BindView(R.id.txtCurrentLoc)
+    TextView currentLoc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,31 +75,64 @@ public class NewEventActivity extends AppCompatActivity implements TimePickerDia
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
-                Intent eventIntent = new Intent();
-                PlaceEvent event = new PlaceEvent();
-                event.setmName(name.getText().toString());
-                event.setmOwner(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
-                event.setDate(fmt.print(date));
-                event.setTime(fmt.print(time));
-                //TODO add fields to event.
-                Parcelable parcel = Parcels.wrap(event);
-                eventIntent.putExtra("EVENT", parcel);
-                setResult(RESULT_OK, eventIntent);
-                finish();
+        fab.setOnClickListener(view -> {
+            DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
+            Intent eventIntent = new Intent();
+            PlaceEvent event = new PlaceEvent();
+            event.setmName(name.getText().toString());
+            event.setmOwner(FirebaseAuth.getInstance().getCurrentUser().getDisplayName());
+            event.setDate(fmt.print(date));
+            event.setTime(fmt.print(time));
+            event.setPlace(currentPlace);
+            //TODO add fields to event.
+            Parcelable parcel = Parcels.wrap(event);
+            eventIntent.putExtra("EVENT", parcel);
+            setResult(RESULT_OK, eventIntent);
+            finish();
+        });
+
+        currentLoc.setText(String.format(getResources().getString(R.string.txt_current_loc), ""));
+
+        dateView.setOnClickListener((click) -> selectDate());
+        timeView.setOnClickListener((click) -> selectTime());
+        useMyLoc.setOnClickListener((click) -> getLocation());
+        selectLoc.setOnClickListener((click) -> openPlacePicker());
+
+    }
+
+    private void getLocation() {
+        MapManager.getInstance().getCurrentPlace().addOnCompleteListener(this, task -> {
+           if (task.isSuccessful()) {
+               String name = task.getResult().get(0).getPlace().getName().toString();
+               currentLoc.setText(String.format(getResources().getString(R.string.txt_current_loc), name));
+               currentPlace = task.getResult().get(0).getPlace();
+           } else {
+               currentLoc.setText(R.string.txt_current_loc_err);
+           }
+        });
+    }
+
+    public void openPlacePicker() {
+        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+        try {
+            startActivityForResult(builder.build(this), 1);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(this, data);
+                String name = place.getName().toString();
+                currentPlace = place;
+                currentLoc.setText(String.format(getResources().getString(R.string.txt_current_loc), name));
             }
-        });
-
-        dateView.setOnClickListener((click) -> {
-            selectDate();
-        });
-
-        timeView.setOnClickListener((click) -> {
-            selectTime();
-        });
+        }
     }
 
     private void selectDate() {
